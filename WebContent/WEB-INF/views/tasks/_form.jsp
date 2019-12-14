508708db34dd5b350174297306343ea8d9eb2608
takashi.nakano <gmcncppbpaucecbh@aol.com>
2019-12-14 21:54:20 +0900

    Add tasklist new

---
+++ WebContent/WEB-INF/views/tasks/_form.jsp
@@ -0,0 +1,10 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<label for="content">タスク</label><br />
+<input type="text" name="content" value="${tasks.content}" />
+
+<br /><br />
+<input type="hidden" name="_token" value="${_token}" />
+<button type ="submit">登録</button>
+
--- src/controllers/NewServlet.java
+++ src/controllers/NewServlet.java
@@ -2,11 +2,14 @@ package controllers;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import models.Tasks;
 
 /**
  * Servlet implementation class NewServlet
@@ -27,6 +30,13 @@ public class NewServlet extends HttpServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("_token" , request.getSession().getId());
+
+        request.setAttribute("task", new Tasks());
+
+       RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/views/tasks/new.jsp");
+       rd.forward(request,response);
+
 
 
     }
